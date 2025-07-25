4a9187fb74941115be78b5c47e41e19cdfd7e03b
Ahmed Nabil <ahmed.nabil@kazyon.com>
2025-07-25 15:02:18 +0300

    Added empty project with splash

---
+++ app/src/main/java/com/route/todoappc42/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.route.todoappc42
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 2000)
+    }
+}
